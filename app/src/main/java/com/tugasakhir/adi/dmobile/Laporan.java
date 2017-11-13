70a16cb3518277a0bc586492289679f82e9e714b
Laurensius Dede Suhardiman <burglar.tea@gmail.com>
2017-11-13 14:43:54 +0700

    Menuju puncak..

--- app/src/main/java/com/tugasakhir/adi/dmobile/Laporan.java
+++ app/src/main/java/com/tugasakhir/adi/dmobile/Laporan.java
@@ -1,11 +1,19 @@
 package com.tugasakhir.adi.dmobile;
 
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.webkit.DownloadListener;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -13,9 +21,21 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Laporan extends AppCompatActivity {
     String[] Ketinggian;
@@ -23,22 +43,36 @@ public class Laporan extends AppCompatActivity {
     String[] Volume;
     String[] Tanggal;
     boolean loaddata=false;
-    Spinner spBulanAwal,spBulanAkhir,spTahunAwal,spTahunAkhir;
-    Button btnCek;
-    String urlLaporan;
+    Spinner spTanggalAwal,spTanggalAkhir,spBulanAwal,spBulanAkhir,spTahunAwal,spTahunAkhir;
+    Button btnCek,btnDownload;
+    public static String urlLaporan;
+    public String nama_laporan;
     String url;
     LinearLayout llLayoutBox;
     int report_length;
+    WebView wvDownload;
+    TextView tvLaporan;
+
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laporan);
+        tvLaporan = (TextView) findViewById(R.id.tvLaporan);
+        wvDownload = (WebView) findViewById(R.id.wvdownload);
+        spTanggalAwal = (Spinner) findViewById(R.id.spTanggalAwal);
+        spTanggalAkhir = (Spinner) findViewById(R.id.spTanggalAkhir);
         spBulanAwal = (Spinner) findViewById(R.id.spBulanAwal);
         spBulanAkhir = (Spinner) findViewById(R.id.spBulanAkhir);
         spTahunAwal = (Spinner) findViewById(R.id.spTahunAwal);
         spTahunAkhir = (Spinner) findViewById(R.id.spTahunAkhir);
         btnCek = (Button) findViewById(R.id.btnCek);
+        btnDownload = (Button) findViewById(R.id.btnDownload);
+        ArrayAdapter<CharSequence> tanggalAdapter = ArrayAdapter.createFromResource(this,R.array.tanggal, android.R.layout.simple_spinner_item);
+        tanggalAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spTanggalAwal.setAdapter(tanggalAdapter);
+        spTanggalAkhir.setAdapter(tanggalAdapter);
         ArrayAdapter<CharSequence> bulanAdapter = ArrayAdapter.createFromResource(this,R.array.bulan, android.R.layout.simple_spinner_item);
         bulanAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spBulanAwal.setAdapter(bulanAdapter);
@@ -50,26 +84,24 @@ public class Laporan extends AppCompatActivity {
         btnCek.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String tglAwal = spTanggalAwal.getSelectedItem().toString();
+                String tglAkhir = spTanggalAkhir.getSelectedItem().toString();
                 String indexBulanAwal = spBulanAwal.getSelectedItem().toString();
                 String indexBulanAkhir = spBulanAkhir.getSelectedItem().toString();
                 String tahunAwal = spTahunAwal.getSelectedItem().toString();
                 String tahunAkhir = spTahunAkhir.getSelectedItem().toString();
-                cekFormatLaporan(indexBulanAwal,tahunAwal,indexBulanAkhir,tahunAkhir);
+                cekFormatLaporan(tglAwal,indexBulanAwal,tahunAwal,tglAkhir,indexBulanAkhir,tahunAkhir);
             }
         });
     }
 
-    void cekFormatLaporan(String indexBulanAwal,String tahunAwal,String indexBulanAkhir,String tahunAkhir){
-        urlLaporan = tahunAwal.concat("-").concat(indexBulanAwal).concat("/").concat(tahunAkhir).concat("-").concat(indexBulanAkhir);
-        if(Integer.parseInt(tahunAwal) > Integer.parseInt(tahunAkhir)){
-            Toast.makeText(getApplicationContext(),"Pastikan bulan dan tahun awal lebih kecil/sama dengan bulan tahun akhir. Terima kasih",Toast.LENGTH_LONG).show();
-        }else
-        if(Integer.parseInt(tahunAwal) == Integer.parseInt(tahunAkhir)){
-            if(Integer.parseInt(indexBulanAwal) > Integer.parseInt(indexBulanAkhir)){
-                Toast.makeText(getApplicationContext(),"Pastikan bulan dan tahun awal lebih kecil/sama dengan bulan tahun akhir. Terima kasih",Toast.LENGTH_LONG).show();
-            }else{
-                new GetJSONReport().execute();
-            }
+    void cekFormatLaporan(String tglAwal,String indexBulanAwal,String tahunAwal,String tglAkhir,String indexBulanAkhir,String tahunAkhir){
+        urlLaporan = tahunAwal.concat("-").concat(indexBulanAwal).concat("-").concat(tglAwal).concat("/").concat(tahunAkhir).concat("-").concat(indexBulanAkhir).concat("-").concat(tglAkhir);
+        nama_laporan = tahunAwal.concat("-").concat(indexBulanAwal).concat("-").concat(tglAwal).concat("_").concat(tahunAkhir).concat("-").concat(indexBulanAkhir).concat("-").concat(tglAkhir);
+        int awal = Integer.parseInt(tahunAwal.concat(indexBulanAwal).concat(tglAwal));
+        int akhir = Integer.parseInt(tahunAkhir.concat(indexBulanAkhir).concat(tglAkhir));
+        if(akhir < awal){
+            Toast.makeText(getApplicationContext(),"Pastikan tanggal awal lebih kecil/sama dengan tanggal akhir. Terima kasih",Toast.LENGTH_LONG).show();
         }else{
             new GetJSONReport().execute();
         }
@@ -101,7 +133,7 @@ public class Laporan extends AppCompatActivity {
                         try{
                             for(int x=0;x<report_length;x++){
                                 JSONObject obj_custom_morning_report = custom_morning_report.getJSONObject(x);
-                                Tanggal[x] = obj_custom_morning_report.getString("date");
+                                Tanggal[x] = obj_custom_morning_report.getString("datetime");
                                 Ketinggian[x] = obj_custom_morning_report.getString("ketinggian").substring(0,5);
                                 Status[x] = obj_custom_morning_report.getString("status");
                                 Volume[x] = obj_custom_morning_report.getString("volume").substring(0,7);
@@ -112,10 +144,10 @@ public class Laporan extends AppCompatActivity {
                         }
                     }
                 } catch (final JSONException e) {
+                    loaddata=false;
                     Log.e(LaurensiusSystemFramework.TAG, e.getMessage());
                 }
-            }
-            else{
+            }else{
                 loaddata=false;
             }
             Log.d(LaurensiusSystemFramework.TAG, "JSON data : " + JSON_data);
@@ -126,6 +158,31 @@ public class Laporan extends AppCompatActivity {
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
             if (loaddata) {
+                tvLaporan.setText("Laporan tersedia dan siap di download, klik tombol di bawah untuk melakukan download");
+                btnDownload.setVisibility(View.VISIBLE);
+                btnDownload.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        wvDownload.getSettings().setJavaScriptEnabled(true);
+                        wvDownload.getSettings().setDomStorageEnabled(true);
+                        wvDownload.loadUrl(getResources().getString(R.string.default_server).concat(getResources().getString(R.string.download_custom_report)).concat(urlLaporan));
+                        wvDownload.setDownloadListener(new DownloadListener() {
+                            @Override
+                            public void onDownloadStart(String url, String userAgent,
+                                                        String contentDisposition, String mimetype,
+                                                        long contentLength) {
+                                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, nama_laporan+".pdf");
+                                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                                dm.enqueue(request);
+                                Toast.makeText(getApplicationContext(), "Download laporan :" + nama_laporan + ".pdf" ,Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                });
+
                 llLayoutBox = (LinearLayout)findViewById(R.id.llLayoutBox);
                 if(llLayoutBox.getChildCount() > 0){
                     llLayoutBox.removeAllViews();
@@ -163,7 +220,6 @@ public class Laporan extends AppCompatActivity {
                     //------------end of kiri
 
                     //-----------kanan
-
                     LinearLayout.LayoutParams laparItemKanan = new LinearLayout.LayoutParams(
                             LinearLayout.LayoutParams.MATCH_PARENT,
                             LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -206,10 +262,12 @@ public class Laporan extends AppCompatActivity {
                     llItemKanan.addView(tvItemVolume,laparItemVolume);
                     llItemKanan.addView(tvItemStatus,laparItemStatus);
                 }
-
             }else{
-                Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
+                btnDownload.setVisibility(View.GONE);
+                tvLaporan.setText("Laporan pada periode tersebut tidak tersedia!");
+//                Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
             }
         }
     }
+
 }
